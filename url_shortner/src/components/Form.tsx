d3bcec1b67725d0b6840e60698914cb0a3e96223
etornam45 <navidben45@gmail.com>
2023-08-12 15:27:36 +0000

    Form component for url

---
+++ url_shortner/src/components/Form.tsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <div className="form">
+      <input type="text" placeholder="Enter url here 🔗"/>
+      <button>shortern</button>
+    </div>
+  );
+}
