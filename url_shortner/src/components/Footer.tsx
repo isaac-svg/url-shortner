1dbf4e6add8ff5a22834850f1e7aa2632a3b692b
etornam45 <navidben45@gmail.com>
2023-08-12 15:26:32 +0000

    created and added Footer Component

---
+++ url_shortner/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import "../styles/Footer.css"
+
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <h3>uShort</h3>
+        <p>
+          The open source software to shortern <b>url</b>
+        </p>
+      </footer>
+    </>
+  );
+}
