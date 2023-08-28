06e0e0be9aa5030f7ab3f77b2d65be9693c43827
isaac-svg <isaacsakyi009@gmail.com>
2023-08-28 03:20:21 +0000

    Handling Copy to clipboard

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,75 @@
-import { useState } from "react"
+import { FormEvent, useEffect, useRef, useState } from "react"
+import axios from "axios"
+import clipboardCopy from "clipboard-copy"
+import useLocalStorage from "../utils/useLocalStorage"
 import "../styles/form.css"
 const Form = () => {
   const [isError,setIsError] =  useState<boolean>(false)
+  const {addToStorage, getAllData} = useLocalStorage()
+  const [copy,setCopy] = useState<boolean>(false)
+  const formRef = useRef<HTMLInputElement>(null)
+  const urldataRef = useRef<HTMLSpanElement>(null)
+
+  interface urlObj{
+    origUrl:string,
+    shortUrl:string
+}
+  const urlData:urlObj = {
+    origUrl: "",
+    shortUrl: ""
+  }
+  const  [urls, setUrl] = useState<urlObj[]>([urlData])
+
+  useEffect(()=>{
+    // addToStorage({origUrl:"Helllo", shortUrl:"dsfjkdsfsqj"})
+    // addToStorage({origUrl:"Helllo", shortUrl:"dsfjkdsfs2hj"})
+    // addToStorage({origUrl:"Helllo", shortUrl:"dsfjkdsfsreh"})
+
+    const urls = getAllData()
+    setUrl(urls)
+    
+  },[])
+  const handleSubmit = (e:FormEvent)=>{
+    e.preventDefault()
+    const data:string = formRef.current?.value!
+    generateURL(data)
+  }
+
+const copyToBoard =  (shortUrl:string)=>{
+  setCopy(true)
+  if (urldataRef.current) {
+    clipboardCopy(shortUrl)
+  }
+  setTimeout(()=>{
+    setCopy(false)
+  },3000)
+
+}
+  const generateURL = async (url:string)=>{
+  try {
+    const result =  await  axios.post("http://localhost:8080",{url},{
+      withCredentials:true,
+      headers:{
+        "Content-Type":"application/json"
+      }
+
+       
+    })
+
+  const data =   result.data
+    addToStorage(data)
+    const res = getAllData()
+    setUrl(res)
+  } catch (error) {
+    console.log(error)
+  }
+  }
   return (
     <section className="form_wrapper">
       <div className="form_box">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="input_container">
-        <input type="text" placeholder="shorten a link here..." className={`input_field ${isError  && "input_error"}`} />
+        <input ref={formRef} type="text" placeholder="shorten a link here..." className={`input_field ${isError  && "input_error"}`} />
         {isError && <span className="error_msg" > Please add a link</span>}
         </div>
     <button className="form_btn">Shorten it!</button>
@@ -15,29 +77,33 @@ const Form = () => {
       </div>
 
       <div className="outputs">
-        <div className="single_output">
+        {
+          urls?.map((url)=>
+<div className="single_output" key={url.shortUrl}>
           <div className="original_link-wrapper">
           <span className="original_link">
-            https://localhost:3000/valence.
+           {url.origUrl}
           </span>
           </div>
           <div className="horizontal_line"></div>
           <div className="shortened_link-container">
             
             <div className="shortened_link-wrapper">
-            <span className="shortend_link">
-              https://we3.rewasfdsafdsfdsfds
+            <span className="shortend_link" ref={urldataRef}>
+              {url.shortUrl}
             </span>
             </div>
            
           
            <div>
-           <button className="copy_btn">Copy</button>
+           <button className={`copy_btn ${ copy && `copied`}`} onClick={()=>copyToBoard(url.shortUrl)}>{copy ? "Copied!" : "Copy"}</button>
            </div>
           </div>
         </div>
+          )
+        }
         
-        <div className="single_output">
+        {/* <div className="single_output">
           <div className="original_link-wrapper">
           <span className="original_link">
             https://localhost:3000/valence.
@@ -79,7 +145,7 @@ const Form = () => {
            <button className="copy_btn">Copy</button>
            </div>
           </div>
-        </div>
+        </div> */}
 
         
       </div>
--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -3,10 +3,7 @@ interface urlObj{
     origUrl:string,
     shortUrl:string
 }
-interface storageObj{
-    _id:string,
-    value:string,
-}
+
 const addToStorage = (data:urlObj) : void=>{
 
     const _id: string = Date().toString()
@@ -19,25 +16,26 @@ const removeFromStorage = (_id:string) : void =>{
     localStorage.removeItem(_id);
 }
 
-const getAllData = () : Record<string, string>[] =>{ 
+const getAllData = () : urlObj[] =>{ 
 
-    const allData :Record<string, string> = {}
+    const allData :urlObj[] = []
     const keys: string[] = Object.keys(localStorage);
 
-    const result:Record<string, string>[] = [];
   
     keys.forEach(key=>{
      const data =   localStorage.getItem(key) || ""
-        allData[key] = data
+     console.log(data, "data")
+     const jsonData = JSON.parse(data)
+        allData.unshift(jsonData)
         
     })
-     result.push(allData)
+     
 
-     return result;
+     return allData;
 
 }
 const useLocalStorage = ()=>{
-    return [addToStorage, removeFromStorage, getAllData]
+    return {addToStorage, removeFromStorage, getAllData}
 }
 
 export default useLocalStorage
